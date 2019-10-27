53dd477741ac600c2787451717746d75afb559b1
unknown <khoirulhudaid@gmail.com>
2019-10-27 07:43:27 +0700

    second

--- config.php
+++ config.php
@@ -1,11 +1,10 @@
 <?php
 
-$username = "root";
-$password = "";
-$database = "tugaspbo";
-$hostname = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "tugaspbo";
+// $hostname = "localhost";
 
-$connection = mysql_connect($hostname, $username, $password);
-mysql_select_db($database);
-
+// $connection = mysql_connect($hostname, $username, $password);
+// mysql_select_db($database);
 ?>
--- index.php
+++ index.php
@@ -9,19 +9,79 @@ include_once("config.php");
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css.css">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+
 <body>
     <header>
-        <nav>
-            <button>Sign In</button>
-            <button>Sign Up</button>
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark ">
+          <a class="navbar-brand text-light bg-dark" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto ">
+              <li class="nav-item active">
+                <button type="button" class="btn btn-light">Sign In</button>
+                </li>
+              <li class="nav-item">
+                <!-- <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> -->
+                <button type="button" class="btn btn-light">Sign Up</button>
+              </li>
+            </ul>
+            
+          </div>
         </nav>
     </header>
+
     <section>
-        <div class = "container">
-            <form action="Username" method="post"><textarea name="Username" id="" cols="30" rows="2">Username</textarea></form>
-            <form action="Email" method="post"><textarea name="Email" id="" cols="30" rows="2">Email</textarea></form>
-        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 mt-5">
+               <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+                     <div class="carousel-inner">
+                        <div class="carousel-item active">
+                         <img src="image/dairy-book-9424595.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="image/2.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="..." class="d-block w-100" alt="...">
+                        </div>
+                      </div>
+                      <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </div>
+                  </div>
+                <div class="col-sm-4 mt-5">
+                    <form>
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                      </div>
+                      <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                      </div>
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                    </div>
+                  </div>
+                </div>
+                </div>
+            </div>
     </section>
 </body>
 </html>
