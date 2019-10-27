16d48a687d9877b238832238914c9817614b7bb5
unknown <khoirulhudaid@gmail.com>
2019-10-28 02:24:55 +0700

    sign up

--- index.php
+++ index.php
@@ -10,6 +10,10 @@ include_once("config.php");
     <link rel="stylesheet" href="css.css">
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -42,13 +46,10 @@ include_once("config.php");
                <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
                      <div class="carousel-inner">
                         <div class="carousel-item active">
-                         <img src="image/dairy-book-9424595.jpg" class="d-block w-100" alt="...">
+                         <img style="max-height: 379px; max-width: 100%;" src="image/dairy-book-9424595.jpg" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
-                          <img src="image/2.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                          <img src="..." class="d-block w-100" alt="...">
+                          <img style="max-height: 379px; max-width: 100%;" src="image/2.jpg" class="d-block w-100" alt="...">
                         </div>
                       </div>
                       <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
@@ -59,10 +60,14 @@ include_once("config.php");
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="sr-only">Next</span>
                       </a>
-                    </div>
+                </div>
                   </div>
                 <div class="col-sm-4 mt-5">
                     <form>
+                     <div class="form-group">
+                        <label for="exampleInputEmail1">Username</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username">
+                      </div>
                       <div class="form-group">
                         <label for="exampleInputEmail1">Email address</label>
                         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
@@ -71,6 +76,11 @@ include_once("config.php");
                         <label for="exampleInputPassword1">Password</label>
                         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                       </div>
+                      <div class="form-group">
+                        <label for="exampleInputPassword1">Re Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Re Password">
+                      </div>
+
                       <div class="form-group form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1">
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
