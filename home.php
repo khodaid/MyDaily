56a1509bdde19bd8508bc43ef5aa7a85dd8938aa
khoirul-06990 <khoirulhudaid@gmail.com>
2019-12-22 19:48:21 +0700

    profil

--- home.php
+++ home.php
@@ -25,10 +25,9 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css.css">
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="./assets/css/css.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -36,80 +35,89 @@ if(isset($_POST['submit'])){
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark ">
-          <a class="navbar-brand text-light bg-dark" href="#">Navbar</a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-info ">
+          <a class="navbar-brand text-light bg-info" href="#">Navbar</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto ">
-              <li class="nav-item active">
-                <button type="button" class="btn btn-light">Sign In</button>
-                </li>
-              <li class="nav-item">
-                <!-- <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> -->
-                <button type="button" class="btn btn-light">Sign Up</button>
+          <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn badge-pill btn-outline-dark btn my-2 my-sm-0" type="submit">Search</button>
+          </form>
+
+          <div class="collapse navbar-collapse" id="navbar-list-4">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active ml-8">
+              <a class="nav-link ml-10" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active ml-8">
+              <a class="nav-link ml-10" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active ml-8">
+              <a class="nav-link ml-10" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+          
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown ml-auto mr-4">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <img src = "image/daily.png" class="mr-0 mt-0 rounded-circle" style="width:45px;">
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Dashboard</a>
+                    <a class="dropdown-item" href="#">Edit Profile</a>
+                    <a class="dropdown-item" href="#">Log Out <img src="image/icon logout.svg" style="width:20px;"></a>
+                  </div>
               </li>
             </ul>
-            
           </div>
         </nav>
     </header>
 
-    <section>
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-8 mt-5">
-               <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-                     <div class="carousel-inner">
-                        <div class="carousel-item active">
-                         <img style="max-height: 379px; max-width: 100%;" src="image/dairy-book-9424595.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                          <img style="max-height: 379px; max-width: 100%;" src="image/2.jpg" class="d-block w-100" alt="...">
-                        </div>
-                      </div>
-                      <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                      </a>
-                      <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                      </a>
+      <div class = "container-fluid">
+        <div class = "row">
+          <div class="col-sm-3 mt-3 section">
+              <img src = "image/daily.png" class = "image text-center" alt = "" style = "max-width : 200px">
+          </div>
+          <div class="col-sm-6 mt-3 rounded-lg content">
+              <div class = "article">
+                <div class = "thinks rounded-lg">
+                  <h4>Apa yang anda pikirkan</h3>
+                  <textarea id = "main"></textarea>
+                    <div class = "custom-file">
+                      <label for = "file-input">
+                        <img src = "image/Icon/camera.png" class = "icon">
+                      </label>
+                      <input type = "file" id = "file-input">
+                    <button type="button" class="btn btn-primary posting ml-4 float-md-right">Posting</button>
+                    </div>
                 </div>
-                  </div>
-                <div class="col-sm-4 mt-5">
-                    <form action="home.php" method="post">
-                     <div class="form-group">
-                        <label for="username">Username</label>
-                        <input type="text" name="username" class="form-control" placeholder="Username">
-                      </div>
-                      <!-- <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                      </div> -->
-                      <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" name="password" placeholder="Password">
-                      </div>
-                      <!-- <div class="form-group">
-                        <label for="exampleInputPassword1">Re Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Re Password">
-                      </div> -->
-
-                      <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                      </div>
-                      <input class="btn btn-primary" type="submit" name="submit" value="submit">
-                    </form>
+              </div>
+              <div class = "article">
+                <div class = "thinks rounded-lg">
+                  <div class="media">
+                    <img class="mr-3 img-thumbnail img-fluid" src="image/daily.png" alt="Generic placeholder image">
+                    <div class="media-body">
+                      <h5 class="mt-0">Media heading</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                     </div>
                   </div>
                 </div>
+              </div>
+              <div class = "article">
+                <div class = "thinks rounded-lg">
+                  <div class="media">
+                    <img class="mr-3 img-thumbnail img-fluid" src="image/daily.png" alt="Generic placeholder image">
+                    <div class="media-body">
+                      <h5 class="mt-0">Media heading</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                  </div>
                 </div>
-            </div>
-    </section>
+              </div>
+          </div>
+        </div>
+      </div>
 </body>
 </html>
